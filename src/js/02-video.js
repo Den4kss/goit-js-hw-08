19c77dc5b7c542aac6807502f804e226a68b275a
Den4kss <106112858+Den4kss@users.noreply.github.com>
2022-10-18 13:29:44 +0300

    2 fin

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const secondsLocal = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(secondsLocal)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+player.on(
+  'play',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 100)
+);
